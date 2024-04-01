97e8923e84d79ccdc32d2e621fb411f6bda73150
Brendo Henrique <61010089+BLurkE@users.noreply.github.com>
2024-04-01 09:58:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+//Script 1
+// alert("Hello World")
+
+//Script 2
+// let num1 = 3
+// let num2 = 7
+// alert(num1 + num2)
+
+//Script 3
+// let tipeOf = 2
+// let check = (typeof(tipeOf)) == "number"
+// if(check){
+//   alert("É um número")
+// }
+// else{
+//   alert("Não é um número")
+// }
+
+//Script 4
+// let tipeOf = "23"
+// let check = (typeof(tipeOf)) == "string"
+// if(check){
+//   alert("É uma string")
+// }
+// else{
+//   alert("Não é uma string")
+// }
+
+//Script 5
+// let tipeOf = true
+// let check = (typeof(tipeOf)) == "boolean"
+// if(check){
+//   alert("É um boolean")
+// }
+// else{
+//   alert("Não é um boolean")
+// }
+
+//Script 6
+// let num1 = 10
+// let num2 = 2
+// alert(num1 - num2)
+
+//Script 7
+// let num1 = 10
+// let num2 = 2
+// alert(num1 * num2)
+
+//Script 8
+// let num1 = 10
+// let num2 = 2
+// alert(num1 / num2)
+
+//Script 9
+// let num1 = 10
+// let check = (num1 % 2) == 0
+// if(check){
+//   alert("É um número par")
+// }
+// else{
+//   alert("Não é um número par")
+// }
+
+//Script 10
+// let num1 = 11
+// let check = (num1 % 2) != 0
+// if(check){
+//   alert("É um número ímpar")
+// }
+// else{
+//   alert("Não é um número ímpar")
+// }
+
